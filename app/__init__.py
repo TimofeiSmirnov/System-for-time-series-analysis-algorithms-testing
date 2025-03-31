00ba01cdcbae0e09735306b619345111a25ff18a
TimofeiSmirnov <ts5310m@gmail.com>
2025-04-01 01:13:31 +0300

    Add init

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+from app.config import Config
+from app.routes import init_routes
+
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'data', 'userTS')
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+    init_routes(app)
+
+    return app
